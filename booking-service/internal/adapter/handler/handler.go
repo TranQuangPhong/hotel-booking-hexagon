9ff69fc67a96d0eeaba1c985e68459125128af02
tranquangphongbk <tranquangphongbk@gmail.com>
2026-09-16 02:42:56 +0700

    [booking svc] http handler and router

--- booking-service/internal/adapter/handler/handler.go
+++ booking-service/internal/adapter/handler/handler.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"booking/booking-service/internal/booking"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BookingHandler struct {
+	bookingService *booking.BookingService
+}
+
+func NewBookingHandler(s *booking.BookingService) *BookingHandler {
+	return &BookingHandler{bookingService: s}
+}
+
+func (h *BookingHandler) GetBookings(c *gin.Context) {
+	userID := c.Param("user_id") //TODO: Extract from JWT
+	bookings, err := h.bookingService.GetBookingDetailByUserID(c, userID)
+	if err != nil {
+		c.JSON(500, gin.H{"error": fmt.Errorf("%w", err).Error()})
+		return
+	}
+	c.JSON(200, bookings)
+}
+
+func (h *BookingHandler) GetBookingByID(c *gin.Context) {
+	bookingID := c.Param("id")
+	booking, err := h.bookingService.GetBookingDetailByID(c, bookingID)
+	if err != nil {
+		c.JSON(500, gin.H{"error": fmt.Errorf("%w", err).Error()})
+		return
+	}
+	c.JSON(200, booking)
+}
--- booking-service/internal/adapter/handler/router.go
+++ booking-service/internal/adapter/handler/router.go
@@ -1,1 +1,26 @@
 package handler
+
+import (
+	"net/http"
+
+	logger "github.com/TranQuangPhong/hotel-booking-logger"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *BookingHandler) BookingRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(logger.LoggingMiddleware())
+
+	r.GET("/bookings/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+	v1 := r.Group("/bookings/api/v1")
+	{
+		v1.GET("/", h.GetBookings)
+		v1.GET("/:id", h.GetBookingByID)
+		// v1.DELETE("/:id", h.CancelBooking)
+	}
+
+	return r
+}
